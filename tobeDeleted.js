b8edec6b2a8db27ea46e2b2dbba85bd303f9d47e
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-08-12 11:52:06 +0100

    final commit

--- tobeDeleted.js
+++
@@ -1,5 +0,0 @@
-getUsefulString('Chike Ft Ric Hassani')
-function getUsefulString(string) {
-  string = string.split('').slice(0, string.indexOf('Ft')).join('').trim();
-  return(string, string.length)
-}
